20caa01c107c83df5c07ce57218468bd613de398
Jzdoot <9559123-jzdoot@users.noreply.gitlab.com>
2024-09-09 23:57:20 -0400

    quick change to test

--- widgets/mpd/mpd.lua
+++ widgets/mpd/mpd.lua
@@ -6,7 +6,7 @@ local volume, title, status = "", "", ""
 
 local host = os.getenv("MPD_HOST") or "localhost"
 local port = os.getenv("MPD_PORT") or "6600"
-local function update_widget()
+local function update_widget(volume ,status)
 	local text = title
 	if status == "pause" then
 		text = text .. " ([󰏤] paused) "
@@ -28,8 +28,8 @@ connection = mpc.new(host , port, nil, error_handler,"currentsong", function (_,
 -- connection = mpc.new("localhost", nil, nil, error_handler,"currentsong", function (_, result)
 	title = result.title
 end, "status", function(_, result)
-	volume = result.volume
-	status = result.state
-	pcall(update_widget)
+	local vol = result.volume
+	local stat = result.state
+	pcall(update_widget(vol,stat))
 end)
 return mpcWidget
